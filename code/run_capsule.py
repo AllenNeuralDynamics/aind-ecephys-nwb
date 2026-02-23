4f4bbf480a167244e3698d5ffc1eeef647052735
Alessio Buccino <alessio.buccino@alleninstitute.org>
2026-02-23 10:44:43 +0100

    N_JOBS_EXT to control external n_jobs (#17)

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -181,8 +181,8 @@ if __name__ == "__main__":
         else:
             SURFACE_CHANNEL_AGAR_PROBES_INDICES = None
 
-    # Use CO_CPUS/SLURM_CPUS_ON_NODE env variable if available
-    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_CPUS_ON_NODE")
+    # Use CO_CPUS/N_JOBS_EXT env variable if available
+    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("N_JOBS_EXT")
     N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
     job_kwargs = dict(n_jobs=N_JOBS, progress_bar=False, mp_context="spawn")
     si.set_global_job_kwargs(**job_kwargs)
